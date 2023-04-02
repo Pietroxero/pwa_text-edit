f3563555783452515d1bd95d075b5f0d0c58d8e4
Pietroxero <115331791+Pietroxero@users.noreply.github.com>
2023-04-01 22:26:38 -0700

    finished database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,5 +18,19 @@ const contactDb = await openDb ('jate', 1);
 const tx = contactDb.transaction ('jate', 'readwrite');
 const store = tx.objectStore ('jate');
 const request = store.put ({ id: 1, value: content });
+const result = await request;
+console.log ('Donna Noble has been Saved', result);
+};
 
-}
+export const getDb = async () => {
+    console.log ('GET from the Rebels database');
+    const contactDb = await openDb ('jate', 1);
+    const tx = contactDb.transcaction ('jate', 'readonly');;
+    const store = tx.objectStore ('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log ('result.value', result);
+    return result?.value;
+};
+
+initdb();
