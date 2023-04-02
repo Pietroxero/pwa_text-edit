702c52c501a888395ff7c79a4c799a43e507bbff
Pietroxero <115331791+Pietroxero@users.noreply.github.com>
2023-04-01 18:27:59 -0700

    added in code for TODO within webpack.config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,19 +11,64 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      //here is where we add webpack to generate our html file
+      new HtmlWebpackPlugin ({
+        template: './index.html',
+        title: 'JATE'
+      }),
+      // this will inject our service worker
+      new InjectManifest ({
+        swSrc: './src-sw.js',
+        seDest: 'src-sw.js',
+      }),
+      // here we create the manifest.json file
+      new WebpackPwaManifest ({
+        fingerprints: false,
+        inject: true,
+        name: 'Here is yet ANOTHER text editor YAY',
+        short_name: 'JATE',
+        description: 'Here is yet ANOTHER text editor YAY',
+        background_color: '225ca3',
+        theme_color: '225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [{
+            src: path.resolve ('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.json ('assets', 'icons'),
+      },
+    ], 
+      }),
     ],
 
     module: {
-      rules: [
-        
+        //here will be our css loaders
+
+      rules: [{
+test: /\.css$/i,
+use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset.env'],
+                plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+        },
+      },
       ],
     },
   };
